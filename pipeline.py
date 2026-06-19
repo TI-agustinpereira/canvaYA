fe22a52940430ea0a011762796105986b3119276
Agustin Pereira <AguPereira@tiendainglesa.com.uy>
2026-06-19 14:11:04 -0300

    pipeline de ejecucion completo. importa funciones de informacion/segregar.py para separar el excel en dfs utiles para operar

--- pipeline.py
+++ pipeline.py
@@ -5,9 +5,22 @@ Guarda en memoria resultados anteriores y ejecuta el flujo de 2actualizacion/gen
 
 RPA con playwright actualiza las tablas con los .xlsx generados en pasos previos.
 """
+import pandas as pd
+from datetime import datetime
 
-from informacion.segregacion import segregar
+fecha = datetime.now().year
 
-print("corri")
-segregar()
+from informacion.segregacion import generar_df_slide_1, generar_df_slide_2, generar_df_slide_3, generar_df_slide_4, generar_df_slide_5, generar_df_slide_6
+from actualizacion.actualizar_graficas import usar_canva
 
+actual = pd.read_excel(f"informacion/datos/encuesta_clima_{fecha - 1}.xlsx")
+pasado = pd.read_excel(f"informacion/datos/encuesta_clima_{fecha - 2}.xlsx")
+
+diferencias_1, path_1 = generar_df_slide_1(actual, pasado)
+path_2 = generar_df_slide_2(actual, pasado)
+diferencias_2, path_3 = generar_df_slide_3(actual, pasado)
+# generar_df_slide_4(actual, pasado)
+generar_df_slide_5(actual)
+generar_df_slide_6(actual, pasado)
+
+usar_canva([path_1, path_2, path_3], [diferencias_1, diferencias_2])
